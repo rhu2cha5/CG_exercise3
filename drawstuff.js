c77471ca9f2cc2d9f791b6d44279e111d4618ffe
Rhucha <rhucha.deodhar@gmail.com>
2026-09-09 17:46:29 -0400

    changed light pos

--- drawstuff.js
+++ drawstuff.js
@@ -391,10 +391,10 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Define a rectangle in 2D with colors and coords at corners
-    var globals = { lightPos: new Vector(150,100,10),  // light over left upper rect
+    var globals = { lightPos: new Vector(150,100,5),  // light over left upper rect
                     lightCol: new Color(255,255,255)}; // light is white
     var tlAttribs = { diffuse: new Color(0,0,255)};    // all four rect verts blue
-    var trAttribs = { diffuse: new Color(0,0,255)};
+    var trAttribs = { diffuse: new Color(255,0,255)};
     var brAttribs = { diffuse: new Color(0,0,255)};
     var blAttribs = { diffuse: new Color(0,0,255)};
     interpRect(imagedata,50,150,50,200,globals,tlAttribs,trAttribs,brAttribs,blAttribs);
