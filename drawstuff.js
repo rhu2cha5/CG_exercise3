b22594488daca47c4e0ee95de200ff0c3f2fbd7f
Rhucha <rhucha.deodhar@gmail.com>
2026-09-09 18:18:52 -0400

    changed color of light

--- drawstuff.js
+++ drawstuff.js
@@ -392,7 +392,7 @@ function main() {
  
     // Define a rectangle in 2D with colors and coords at corners
     var globals = { lightPos: new Vector(150,100,10),  // light position
-                    lightCol: new Color(255,0,255)}; // light color
+                    lightCol: new Color(255,192,203)}; // light color
     var tlAttribs = { diffuse: new Color(0,0,255)};    // all four rect verts blue
     var trAttribs = { diffuse: new Color(0,0,255)};
     var brAttribs = { diffuse: new Color(0,0,255)};
